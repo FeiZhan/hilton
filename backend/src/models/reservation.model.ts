cbae59ef28a6daf1a1167a173d1d4714659ae756
Fei Zhan <fei.zhan.zju@gmail.com>
2025-04-04 14:31:12 +0800

    Configure model

---
+++ backend/src/models/reservation.model.ts
@@ -0,0 +1,45 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Reservation extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  guestName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  guestContactInfo: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  expectedArrivalTime: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  reservedTableSize: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  status: string;
+
+
+  constructor(data?: Partial<Reservation>) {
+    super(data);
+  }
+}
+
+export interface ReservationRelations {
+  // describe navigational properties here
+}
+
+export type ReservationWithRelations = Reservation & ReservationRelations;
