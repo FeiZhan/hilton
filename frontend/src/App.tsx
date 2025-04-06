9163b1170eecf6f89ebe614841cd2304160eb0ba
Fei Zhan <fei.zhan.zju@gmail.com>
2025-04-06 21:30:08 +0800

    Add links to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CreateReservationPage from './pages/CreateReservationPage';
 import FilterReservationsPage from './pages/FilterReservationsPage';
@@ -7,6 +7,11 @@ import FilterReservationsPage from './pages/FilterReservationsPage';
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create Reservation</Link>
+        <Link to="/filter">Filter Reservations</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreateReservationPage />} />
