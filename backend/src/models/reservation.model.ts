921fdf94b6fb174e0d37ed710831a21a11b74d3e
Fei Zhan <fei.zhan.zju@gmail.com>
2025-04-08 08:14:40 +0800

    Add model for GraphQL

--- backend/src/models/reservation.model.ts
+++ backend/src/models/reservation.model.ts
@@ -1,4 +1,5 @@
 import {Entity, model, property} from '@loopback/repository';
+import {Field, ObjectType} from 'type-graphql';
 
 @model()
 export class Reservation extends Entity {
@@ -39,10 +40,30 @@ export class Reservation extends Entity {
   })
   status: string;
 
-
   constructor(data?: Partial<Reservation>) {
     super(data);
   }
+}
+
+@ObjectType()
+export class ReservationGraphQL {
+  @Field()
+  id: string;
+
+  @Field()
+  guestName: string;
+
+  @Field()
+  guestContactInfo: string;
+
+  @Field()
+  expectedArrivalTime: string;
+
+  @Field()
+  reservedTableSize: number;
+
+  @Field()
+  status: string;
 }
 
 export interface ReservationRelations {
